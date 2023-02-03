d24273b8f51b0a6b5f6768bf14752113453e8eb1
KeitaShimura <keitashimura2023@gmail.com>
2023-02-04 03:59:40 +0900

    UIの修正

--- index.php
+++ index.php
@@ -18,11 +18,11 @@ $posts = $pdo->query('SELECT * FROM posts ORDER BY id DESC');
 
 <body>
     <h1 class="fs-1" style="margin: 50px 0 0 40px;">TODO一覧、登録画面</h1>
-    <div style="text-align: center;">
+    <div style="text-align: center;" class="position-relative">
         <form method="post" action="add.php">
-            <h2 class="fs-2" style="text-align: left; margin: 50px 0 0 100px;">TODOアプリ</h2>
+            <h2 class="fs-2" style="text-align: left; margin: 50px 0 0 10%;">TODOアプリ</h2>
             <div class="form" style="text-align: center;">
-                <textarea name="content"  rows="4" placeholder="ここにTOTOを入力" style="width:650px"></textarea>
+                <textarea name="content" cols="100" rows="4" placeholder="ここにTOTOを入力" style="width:80%; padding: 5px 10px;"></textarea>
                 <div class="button" style="margin-top: 5px;">
                     <button type="submit" class="btn btn-primary">作成</button>
                 </div>
@@ -32,7 +32,8 @@ $posts = $pdo->query('SELECT * FROM posts ORDER BY id DESC');
             <?php if ($posts->rowCount() === 0) { ?>
                 <h3 class="fs-3" style="text-align: center; margin: 50px 0 0 0;">TODOはありません</h3>
             <?php } else { ?>
-                <table class="table" style="margin:30px auto; text-align: center; border-top: 1px solid lightgray; width:650px;" >
+                <div class="table-responsive">
+                <table class="table" style="margin:30px auto; text-align: center; border-top: 1px solid lightgray; width:80%;" >
                     <thead style="height: 50px;">
                         <tr>
                             <th class="col-3" style="font-weight: bold;">メモの内容</th>
@@ -43,13 +44,14 @@ $posts = $pdo->query('SELECT * FROM posts ORDER BY id DESC');
                     <?php while ($post = $posts->fetch()) : ?>
                         <tbody>
                             <tr>
-                                <td class="col-3" style="text-align: left; line-height: 40px;"><?php print($post['content']); ?></td>
-                                <td class="col-3"><a href="update_do.php?id=<?php print($post['id']); ?>" class="btn btn-primary">編集</a></td>
-                                <td class="col-3"><a href="delete.php?id=<?php print($post['id']); ?>" class="btn btn-danger">削除</a></td>
+                                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($post['content']); ?></td>
+                                <td class="col-3" style="vertical-align: middle;"><a href="update_do.php?id=<?php print($post['id']); ?>" class="btn btn-primary">編集</a></td>
+                                <td class="col-3" style="vertical-align: middle;"><a href="delete.php?id=<?php print($post['id']); ?>" class="btn btn-danger">削除</a></td>
                             </tr>
                         </tbody>
                     <?php endwhile; ?>
                 </table>
+                </div>
             <?php } ?>
         </article>
     </div>
--- update_do.php
+++ update_do.php
@@ -22,10 +22,10 @@ if(isset($_REQUEST['id'])) {
 <body>
     <h1 class="fs-1" style="margin: 50px 0 0 40px;">TODO編集画面</h1>
     <div style="text-align: center;" >
-        <h2 class="fs-1" style="text-align: left; margin: 50px 0 0 100px;">TODOアプリ</h2>
+        <h2 class="fs-1" style="text-align: left; margin: 50px 0 10px 10%;">TODOアプリ</h2>
         <form method="post" action="update.php">
             <input type="hidden" name="id" value="<?php print($id); ?>">
-            <textarea  name="content" rows="4" style="width:650px;"><?php print($post['content']); ?></textarea>
+            <textarea name="content" cols="100" rows="4"  style="width:80%; padding: 5px 10px;"><?php print($post['content']); ?></textarea>
             <div class="button" style="margin-top: 5px;">
                 <a href="index.php" class="btn btn-secondary">戻る</a>
                 <button type="submit" class="btn btn-primary">更新</button>
