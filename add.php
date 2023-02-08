1bfafb0eec62a50a3bf072bc904583a073eadba0
KeitaShimura <keitashimura2023@gmail.com>
2023-02-08 17:20:21 +0900

    フォルダの作成

--- add.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-require "db.php";
-
-$posts = $pdo->prepare('INSERT INTO posts SET content=?, created_at=NOW()');
-$posts->execute(array($_POST['content']));
-
-header('Location: http://localhost/todo/');
-?>
--- update.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "db.php";
-
-$posts = $pdo->prepare('UPDATE posts SET content=?, updated_at=NOW() WHERE id=?');
-$posts->execute(array($_POST['content'], $_POST['id']));
-
-header('Location: http://localhost/todo/');
-?>
